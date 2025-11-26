e1609704c03e9c5b78426ea2fafc20e05d0b9f97
praveenkp24 <praveen.kp@neubergdiagnostics.com>
2025-11-26 12:35:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # allows frontend hosted elsewhere to fetch data
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # enable CORS for all domains
+CORS(app)
 
 latest_payload = {}
 
